d6e657c92345fcd9ddd4c833253fde88c988a8ec
Mask02 <shoaibakbarm@gmail.com>
2023-03-09 22:18:03 +0500

    login page added

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/index")
 def index():
     return render_template('index.html')
 
